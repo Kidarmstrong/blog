f2423148dd2217ca15c41895bcccfd8bf7ae0a31
Sean Armstrong <sw.armstrong@ntlworld.com>
2014-05-26 16:03:07 +0100

    Correct bootstrap. Start image work

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,12 @@ class Post < ActiveRecord::Base
 
   validates :subject, presence: true, length: { maximum: 30 } 
   validates :main_body, presence: true
+
+  #Scopes
+  scope :carousel, lambda { |num| order('created_at DESC').limit(num) }
+
+  def self.newest
+    Post.last
+  end
+
 end
